2311f4f9f40c4e9a7ce1a95b704b027d09a98af5
brokkoli71 <brokkoli71b@gmail.com>
2024-06-05 13:10:15 +0200

    print error output

--- setup.py
+++ setup.py
@@ -131,11 +131,9 @@ class CMakeBuild(build_ext):
         if self.debug:
             print("cmake build: " + str(["cmake", "--trace", "--build", ".", "--target", "_pylibCZIrw"] + build_args))
 
-        #try:
-        subprocess.check_output(["cmake", "--trace", "--build", ".", "--target", "_pylibCZIrw"] + build_args,
-            cwd=self.build_temp, env=env, shell=True, stderr=subprocess.STDOUT)
-        #except subprocess.CalledProcessError as e:
-        #    raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
+        cmake_build_output = subprocess.check_output(["cmake", "--trace", "--build", ".", "--target", "_pylibCZIrw"] + build_args,
+            cwd=self.build_temp, env=env, stderr=subprocess.STDOUT)
+        raise RuntimeError(f"{cmake_build_output=}")
 
 
 def check_and_install_packages(packages: List[str], triplet: str, vcpkg_root: str) -> None:
